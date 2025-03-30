66b4e6251fad101f1de1d1bb9acb3e172fa9f057
MartirosAl <145045481+MartirosAl@users.noreply.github.com>
2025-03-30 20:26:49 +0300

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,26 @@
+#include "hash_tables.h"
+
+int test(int a)
+{
+   return a % 5;
+}
+
+int main()
+{
+   Hash_table_internal A(test, 15, 5);
+   A.insert(0);
+   A.insert(1);
+   A.insert(2);
+   A.insert(3);
+   A.insert(4);
+   //A.Print();
+   A.insert(5);
+   A.insert(10);
+   A.insert(15);
+   A.insert(16);
+   A.insert(17);
+   A.insert(20);
+   A.remove(20);
+   A.Print();
+   return 0;
+}
